790508ee728650734215e6de38da20e25a03932b
Artem Savitskiy <artem.balanar@mail.ru>
2025-09-19 17:58:36 +0300

    сделал метод sayHello

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("GIT Develop");
-        System.out.println("Hello World");
         System.out.println("Im happy");
         System.out.println("Nice");
+        sayHello();
+    }
+    public static void sayHello(){
+        System.out.println("Hello World");
     }
 }
