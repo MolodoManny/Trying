857d113a66a57dbe49259d780aa0cac7041052f9
Artem Savitskiy <artem.balanar@mail.ru>
2025-09-19 18:25:21 +0300

    добавил sout Change

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("GIT Develop");
+        System.out.println("Change");
         System.out.println("Im happy");
         System.out.println("Nice");
         sayHello();
