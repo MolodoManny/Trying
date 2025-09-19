e3048507e57749009b51635ef5d0de7956c8fa04
Artem Savitskiy <artem.balanar@mail.ru>
2025-09-19 18:26:11 +0300

    добавил метод nice

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-
+    nice();
         System.out.println("Im happy");
-        System.out.println("Nice");
         sayHello();
     }
     public static void sayHello(){
         System.out.println("Hello World");
+    }
+    public static void nice(){
+        System.out.println("Nice");
+
     }
 }
