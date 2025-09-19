a1824d3e8b7be6d56c0b15f17efa4842e63967cf
Artem Savitskiy <artem.balanar@mail.ru>
2025-09-19 18:24:14 +0300

    удалил первый sout Hello GIT

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("GIT Develop");
+
         System.out.println("Im happy");
         System.out.println("Nice");
         sayHello();
