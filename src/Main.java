64f9396b06875f17b92bc823f4f878d563954921
Artem Savitskiy <artem.balanar@mail.ru>
2025-09-19 17:56:25 +0300

    измений первый sout GIT Develop

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello GIT");
+        System.out.println("GIT Develop");
         System.out.println("Hello World");
         System.out.println("Im happy");
         System.out.println("Nice");
